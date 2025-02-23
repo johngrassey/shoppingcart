57876d3adb032b99d6caaf0e20ed333762a451da
John Grassey <johngrassey@gmail.com>
2025-02-23 15:38:46 -0500

    Set up CartContext and pass products to it

--- src/App.jsx
+++ src/App.jsx
@@ -2,40 +2,53 @@ import "./App.css";
 import { Outlet } from "react-router-dom";
 import Navbar from "./components/NavBar/Navbar";
 import { useState } from "react";
+import CartContext from "./contexts/CartContext";
 
 function App() {
-  const [cart, setCart] = useState([]);
-  const quantity = cart.length;
-
-  const [products, setProducts] = useState([
+  const [cart, setCart] = useState([
     {
       id: 1,
-      name: "Product 1",
+      name: "Box 1",
       price: 100,
       description: "This is a beautiful box that your loved one will enjoy.",
-      image: "src/assets/diagonal_far.JPG",
+      image: "src/assets/box1.JPG",
+    },
+  ]);
+  const quantity = cart.length;
+
+  const products = [
+    {
+      id: 1,
+      name: "Box 1",
+      price: 100,
+      description: "This is a beautiful box that your loved one will enjoy.",
+      image: "src/assets/box1.JPG",
     },
     {
       id: 2,
-      name: "Product 2",
-      price: 200,
+      name: "Box 2",
+      price: 125,
       description: "This is a beautiful box that your loved one will enjoy.",
-      image: "src/assets/diagonal_far.JPG",
+      image: "src/assets/box2.JPG",
     },
     {
       id: 3,
-      name: "Product 3",
-      price: 300,
+      name: "Box 3",
+      price: 85,
       description: "This is a beautiful box that your loved one will enjoy.",
-      image: "src/assets/diagonal_far.JPG",
+      image: "src/assets/box3.JPG",
     },
-  ]);
+  ];
+
+  const addToCart = (product) => {
+    setCart([...cart, product]);
+  };
 
   return (
-    <>
+    <CartContext.Provider value={{ cart, products, addToCart }}>
       <Navbar quantity={quantity} />
       <Outlet />
-    </>
+    </CartContext.Provider>
   );
 }
 
--- src/components/ShoppingPage/ShoppingPage.jsx
+++ src/components/ShoppingPage/ShoppingPage.jsx
@@ -1,21 +1,17 @@
 import ProductCard from "../ProductCard/ProductCard";
-
-const product = {
-  name: "Product 1",
-  price: 10,
-  image: "src/assets/diagonal_far.JPG",
-  id: 1,
-  description: "This is a beautiful box that your loved one will enjoy.",
-};
+import CartContext from "../../contexts/CartContext";
+import { useContext } from "react";
 
 export default function ShoppingPage() {
+  const { products } = useContext(CartContext);
+
   return (
     <>
       <h2>This is the Shopping Page</h2>
       <div className="flex justify-center gap-10 flex-wrap">
-        <ProductCard product={product} />
-        <ProductCard product={product} />
-        <ProductCard product={product} />
+        {products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
       </div>
     </>
   );
