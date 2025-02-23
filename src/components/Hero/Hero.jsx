413be23dbf3f50e75a43ac8fe2f5259106a3a760
John Grassey <johngrassey@gmail.com>
2025-02-22 22:14:53 -0500

    Update Hero and MainPage components with tailwind
    Deleted uneccessary css module file

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,9 +1,15 @@
-import styles from "./Hero.module.css";
+import { Link } from "react-router-dom";
+import Button from "../Button/Button";
 
 export default function Hero() {
   return (
-    <div className={styles.hero}>
-      <h2>This is the hero.</h2>
+    <div className="p-40 flex flex-col items-center justify-center text-3xl">
+      <h2 className="p-4 text-5xl">
+        Beautiful boxes for life&apos;s precious moments
+      </h2>
+      <Link to="/shoppingpage">
+        <Button name="Shop Now" />
+      </Link>
     </div>
   );
 }
