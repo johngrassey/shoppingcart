25e33daad31683dcc61601dfcc326d8f43d412f9
John Grassey <johngrassey@gmail.com>
2025-02-22 20:57:54 -0500

    Update Hero Styling
    Centered the text for now. Will add more later.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import { Outlet, Link } from "react-router-dom";
 import Navbar from "./components/NavBar/Navbar";
+import Hero from "./components/Hero/Hero";
 
 function App() {
   return (
     <>
       <Navbar />
+      <Hero />
       <Link to="/shoppingpage">Shopping Page</Link>
       <Outlet />
     </>
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,3 +1,9 @@
+import styles from "./Hero.module.css";
+
 export default function Hero() {
-  return <h2>This is the hero.</h2>;
+  return (
+    <div className={styles.hero}>
+      <h2>This is the hero.</h2>
+    </div>
+  );
 }
