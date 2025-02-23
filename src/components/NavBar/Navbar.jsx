f3f5399057964440e095fa4e2eb848158816a866
John Grassey <johngrassey@gmail.com>
2025-02-22 20:57:24 -0500

    Update Navbar Styling and jsx
    Removed the nav tag and cleaned up the classes

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -3,9 +3,9 @@ import { ShoppingCart } from "lucide-react";
 
 export default function Navbar() {
   return (
-    <nav className={styles.navbar}>
+    <header className={styles.header}>
       <h1>BOXWORTHY</h1>
       <ShoppingCart size={36} />
-    </nav>
+    </header>
   );
 }
