5453e7f4eeec3c3d588364f3eff70a0bf63cc035
John Grassey <johngrassey@gmail.com>
2025-02-23 22:31:07 -0500

    Add Empty Cart Logic

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -11,21 +11,33 @@ export default function Checkout() {
 
   return (
     <div className="flex justify-center">
-      <div className="flex flex-col w-3/5 items-center shadow-xl p-10">
+      <div className="flex flex-col w-3/5 items-center shadow-xl p-10 rounded-2xl">
         <h2 className="text-4xl mb-8">Checkout</h2>
-        {cart.map((product) => (
-          <CheckoutCard key={product.id} product={product} />
-        ))}
+        {cart.length === 0 ? (
+          <div
+            role="alert"
+            className="alert alert-error alert-soft w-full mb-5"
+          >
+            <span>Your Cart Is Empty</span>
+          </div>
+        ) : (
+          cart.map((product) => (
+            <CheckoutCard key={product.id} product={product} />
+          ))
+        )}
         <div className="flex flex-col items-end w-full">
           <p className="text-2xl font-bold text-end">
             Grand Total: ${grandTotal}
           </p>
-          <button
-            className="btn btn-primary btn-xl my-5"
-            onClick={() => alert("Thank you for your purchase!")}
-          >
-            Complete Your Purchase
-          </button>
+
+          {cart.length > 0 && (
+            <button
+              className="btn btn-primary btn-xl my-5"
+              onClick={() => alert("Thank you for your purchase!")}
+            >
+              Complete Your Purchase
+            </button>
+          )}
         </div>
       </div>
     </div>
